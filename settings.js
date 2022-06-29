ba43d99e95464c0d6b3ed1a30afe4c9177fb3eaa
JKIshara <108391409+JKIshara@users.noreply.github.com>
2022-06-29 13:25:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,27 +23,27 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94756278003'] //Owner number in aive msg
+global.premium = ['+94756278003'] //Owner Number info
+global.ownernomer = '+94756278003' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '▆▇█ ＩＳＨＡＲＡ █▇▆' //Owner Name
+global.botname = ' ☬𝙎𝙇༒𝘼𝙇𝙋𝙃𝘼 𝘽𝙊𝙏' //Bot Name
 
 global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
 global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬𝙎𝙇༒𝘼𝙇𝙋𝙃𝘼 𝘽𝙊𝙏' //Bot Pacage Name
+global.author = '▆▇█𝙸𝚂𝙷𝙰𝚁𝙰█▇▆' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
 
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
-global.footer = '©_Kumuthu.' // DO NOT CHANGE!
+global.footer = '𝙰𝙻𝙿𝙷𝙰.𝙱𝙾𝚃' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
@@ -53,7 +53,7 @@ global.mess = { // Error Mesages
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👉👈පොඩ්ඩක් ඉන්නෝ...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
